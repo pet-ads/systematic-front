b93d77c49dff982365e111634b05033c36ec5ed2
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2024-12-02 16:29:42 -0300

    feat: update layout of component of visualization in article

--- src/components/Modals/StudyModal/StudyData.tsx
+++ src/components/Modals/StudyModal/StudyData.tsx
@@ -1,4 +1,10 @@
-import { IconButton, Flex, Text, Center, useDisclosure } from "@chakra-ui/react";
+import {
+  IconButton,
+  Flex,
+  Text,
+  Center,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { StudyInterface } from "../../../../public/interfaces/IStudy";
 
 interface IStudyDataFiel {
@@ -17,45 +23,76 @@ export default function StudyDataFiel({ studyData, type }: IStudyDataFiel) {
     flexDirection: "column",
     alignContent: "center",
     overflowY: "scroll",
-    padding: "3"
+    padding: "3",
   };
 
   if (isTypeValid) {
     if (isTypeSelection)
       return (
         <Flex sx={selectionSX}>
-
-            <Flex flexDirection="column" padding="60px">
-
-                <Flex display="flex" lineHeight="1" gap="5" flexDirection="column" w="100%" pt="10px" fontFamily={"Merriweather"}
-                 maxHeight="250px"
+          <Flex flexDirection="column" padding="60px">
+            <Flex
+              display="flex"
+              lineHeight="1"
+              gap="5"
+              flexDirection="column"
+              w="100%"
+              pt="10px"
+              fontFamily={"Merriweather"}
+              maxHeight="250px"
+            >
+              <Flex>
+                <Text marginBottom={"7px"} w="30%" align={"left"}>
+                  <Text fontSize={"14px"} fontWeight={"bold"}>
+                    Type: {studyData.studyType}
+                  </Text>
+                </Text>
+                <Text
+                  fontSize={"20px"}
+                  align={"right"}
+                  as="i"
+                  fontWeight={"Bold"}
+                  w="70%"
                 >
-                  <Flex>
-                    <Text marginBottom={"7px"} w="30%" align={"left"}>
-                          <Text fontSize={"14px"} fontWeight={"bold"}>Type: {studyData.studyType}</Text> 
-                    </Text>
-                    <Text fontSize={"20px"} align={"right"} as="i" fontWeight={"Bold"} w="70%">
-                      {studyData.venue}, {studyData.year}
-                    </Text>
-                  </Flex>
-
-                  <Text fontSize={"35"} fontWeight={"bold"} fontFamily={"Boboni"} lineHeight="1" align={"center"}>
-                    {studyData.title}
-                  </Text>
-
-                  <Text p="1" fontWeight={"Bold"} align={"center"}>
-                    {studyData.authors}
-                  </Text>
-
-                <Flex fontFamily={"Literata"} flexDirection={"column"} align={"right"} gap="15px" pb="60px">
-                    <Text fontSize={"xxl"}><b>Abstract.</b> {studyData.abstract}</Text> 
-                    <Text fontSize={"xxl"}><b>Keywords.</b> {studyData.keywords}</Text> 
-                </Flex>
-
+                  {studyData.venue}, {studyData.year}
+                </Text>
               </Flex>
-                
+
+              <Text
+                fontSize={"35"}
+                fontWeight={"bold"}
+                fontFamily={"Boboni"}
+                lineHeight="1"
+                align={"center"}
+              >
+                {studyData.title}
+              </Text>
+
+              <Text p="1" fontWeight={"Bold"} align={"center"}>
+                {studyData.authors}
+              </Text>
+
+              <Flex
+                fontFamily={"Literata"}
+                flexDirection={"column"}
+                align={"right"}
+                gap="15px"
+                pb="60px"
+              >
+                <Text
+                  fontSize={"xxl"}
+                  textAlign="justify"
+                  lineHeight="1.5rem"
+                  mb="5px"
+                >
+                  <b>Abstract.</b> {studyData.abstract}
+                </Text>
+                <Text fontSize={"xxl"} textAlign="justify" lineHeight="1.5rem">
+                  <b>Keywords.</b> {studyData.keywords}
+                </Text>
+              </Flex>
             </Flex>
-        
+          </Flex>
         </Flex>
       );
   }
