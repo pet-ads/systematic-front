c75d0eadf73f8dc57ba03ebc7bc030051d0cdf74
Leonardo Fattori <leonardo.royale111@gmail.com>
2025-12-16 04:52:57 -0300

    fix(ui): Standardize lateral margins on the 'Identification' page

--- src/features/review/execution-identification/pages/Identification/styles.ts
+++ src/features/review/execution-identification/pages/Identification/styles.ts
@@ -1,6 +1,6 @@
 // styles.ts
 export const conteiner = {
-  mt: 5,
+  mt: "1rem",
   w: "100%", // pega só o espaço disponível no FlexLayout
   h: "calc(100vh - 7.5rem)",
 };
@@ -11,9 +11,8 @@ export const dataBaseconteiner = {
   justifyContent: "center",
   justifyItems: "center",
   boxSizing: "border-box",
-  width: "100%",
-  gap: "2rem",
+  width: "95%",
+  gap: "4rem",
   margin: "0 auto",
-  padding: "0rem",
-  maxHeight: "calc(100vh - 1.5rem)",
+  padding: "1rem",
 };
