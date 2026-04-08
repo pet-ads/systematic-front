70766f21771079bc212fdb0618a48aafab214f04
pedrozzneo <pedrozzneo@gmail.com>
2026-04-08 13:39:41 -0300

    feat: populate the mock data

---
+++ src/mocks/reviewCollaborators.ts
@@ -0,0 +1,17 @@
+export const reviewCollaborators = [
+  {
+    name: "Pedro Neo",
+    email: "pedro.neo@gmail.com",
+    photo: "photo.jpg",
+  },
+  {
+    name: "Captain",
+    email: "captain@gmail.com",
+    photo: "photo.jpg",
+  },
+  {
+    name: "Lucas",
+    email: "lucas@gmail.com",
+    photo: "photo.jpg",
+  },
+];
