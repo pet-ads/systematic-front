6e87f39c4fd617af1c114502d209e7bfb005eb6c
pedrozzneo <pedrozzneo@gmail.com>
2026-04-09 11:05:44 -0300

    feat: populate the potential researchers mock

---
+++ src/mocks/potentialResearchers.tsx
@@ -0,0 +1,17 @@
+export const potentialCollaborators = [
+    {
+      name: "Unincluded Collaborator 1",
+      email: "unincluded1@gmail.com",
+      photo: "photo.jpg",
+    },
+    {
+      name: "Unincluded Collaborator 2",
+      email: "unincluded2@gmail.com",
+      photo: "photo.jpg",
+    },
+    {
+      name: "Unincluded Collaborator 3",
+      email: "unincluded3@gmail.com",
+      photo: "photo.jpg",
+    },
+  ];
