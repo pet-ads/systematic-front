b81983a0e96f02786ae3c1a8eadd093231b7750d
pedrozzneo <pedrozzneo@gmail.com>
2026-04-09 11:32:54 -0300

    feat: add button to add new researchers, for now it just lists the possible ones

--- src/features/review/planning-protocol/pages/GeneralDefinition/subcomponents/ResearcherFilter/index.tsx
+++ src/features/review/planning-protocol/pages/GeneralDefinition/subcomponents/ResearcherFilter/index.tsx
@@ -1,12 +1,19 @@
-import { Text } from "@chakra-ui/react";
+import { Button, Text } from "@chakra-ui/react";
 import { potentialResearchers } from "../../../../../../../mocks/potentialResearchers";
 
 export default function ResearcherFilter() {
   const researchers = potentialResearchers;
 
+  const handleAddCollaborator = () => {
+    researchers.map((researcher) => {
+      console.log(researcher.name);
+    });
+  };
+
   return (
     <>
       <Text mt={"30px"} fontWeight={500} fontSize={"large"}>Researchers</Text>
+      <Button onClick={handleAddCollaborator}>Add Collaborator</Button>
       {researchers.map((researcher) => (  
         <Text key={researcher.name}>{researcher.name} - {researcher.email} - {researcher.photo}</Text>
       ))}
