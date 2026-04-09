99b05c96eedf72d93b90b763787ba69b82e6bac8
pedrozzneo <pedrozzneo@gmail.com>
2026-04-09 14:57:05 -0300

    fix: put different ids to avoid conflicts

--- src/mocks/potentialResearchers.tsx
+++ src/mocks/potentialResearchers.tsx
@@ -1,18 +1,18 @@
 export const potentialResearchersMock = [
   {
-    id: "1",
+    id: "4",
     name: "Potential Researcher 1",
     email: "unincluded1@gmail.com",
     photo: "photo.jpg",
   },
   {
-    id: "2",
+    id: "5",
     name: "Potential Researcher 2",
     email: "unincluded2@gmail.com",
     photo: "photo.jpg",
   },
   {
-    id: "3",
+    id: "6",
     name: "Potential Researcher 3",
     email: "unincluded3@gmail.com",
     photo: "photo.jpg",
