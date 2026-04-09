22bd6b2a654de19075bfc27d615a531b092fd76c
pedrozzneo <pedrozzneo@gmail.com>
2026-04-09 11:34:48 -0300

    feat: populate the mock for added researchers

---
+++ src/mocks/addedResearchers.tsx
@@ -0,0 +1,17 @@
+export const addedResearchers = [
+    {
+      name: "Added Collaborator 1",
+      email: "added1@gmail.com",
+      photo: "photo.jpg",
+    },
+    {
+      name: "Added Collaborator 2",
+      email: "added2@gmail.com",
+      photo: "photo.jpg",
+    },
+    {
+      name: "Added Collaborator 3",
+      email: "added3@gmail.com",
+      photo: "photo.jpg",
+    },
+];
