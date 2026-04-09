6fb0f275e5046ddcb091981bbbc5ee198afa9169
pedrozzneo <pedrozzneo@gmail.com>
2026-04-09 11:23:26 -0300

    feat: change previous not working logic to list the mock of researchers

--- src/features/review/planning-protocol/pages/GeneralDefinition/subcomponents/ResearcherFilter/index.tsx
+++ src/features/review/planning-protocol/pages/GeneralDefinition/subcomponents/ResearcherFilter/index.tsx
@@ -1,46 +1,15 @@
-import { FormControl } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import { FormControlstyle } from "./styles";
-import TextAreaInput from "../../../../../../../components/common/inputs/InputTextArea";
-
-interface IResearcherData {
-  name: string;
-  email: string;
-}
+import { Text } from "@chakra-ui/react";
+import { potentialResearchers } from "../../../../../../../mocks/potentialResearchers";
 
 export default function ResearcherFilter() {
-  const [researchers, setResearchers] = useState<IResearcherData[]>([]);
-  const [filteredResearchers, setFilteredResearchers] = useState<
-    IResearcherData[]
-  >([]);
-  const [inputValue, setInputValue] = useState("");
-
-  useEffect(() => {
-    fetch("src/data/test.json")
-      .then((response) => response.json())
-      .then((res) => setResearchers(res))
-      .catch((err) => console.error(err));
-  }, []);
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const value = e.target.value;
-    setInputValue(value);
-    setFilteredResearchers(
-      researchers.filter((researcher) =>
-        researcher.name.includes(e.target.value)
-      )
-    );
-    console.log(filteredResearchers);
-  };
+  const researchers = potentialResearchers;
 
   return (
-    <FormControl sx={FormControlstyle}>
-      <TextAreaInput
-        label="Researchers"
-        placeholder={"Enter the name of the researchers "}
-        onChange={handleInputChange}
-        value={inputValue}
-      ></TextAreaInput>
-    </FormControl>
+    <>
+      <Text>Researchers</Text>
+      {researchers.map((researcher) => (  
+        <Text key={researcher.name}>{researcher.name} - {researcher.email} - {researcher.photo}</Text>
+      ))}
+    </>
   );
 }
