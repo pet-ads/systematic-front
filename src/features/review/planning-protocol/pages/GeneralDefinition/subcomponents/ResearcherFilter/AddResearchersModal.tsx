ab1f12c2b9627a0387784f5da0ad077263fbd7a5
pedrozzneo <pedrozzneo@gmail.com>
2026-04-09 12:25:17 -0300

    feat: list the potential researchers on the modal

---
+++ src/features/review/planning-protocol/pages/GeneralDefinition/subcomponents/ResearcherFilter/AddResearchersModal.tsx
@@ -0,0 +1,18 @@
+import { Modal, ModalContent, ModalHeader, ModalOverlay } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
+
+type Props = { isOpen: boolean; onClose: () => void; researchers: any[] };
+
+export default function ResearcherFilterModal({ isOpen, onClose, researchers }: Props) {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Potential Researchers</ModalHeader>
+        {researchers.map((researcher) => (
+          <Text key={researcher.email}>{researcher.name} - {researcher.email} - {researcher.photo}</Text>
+        ))}
+      </ModalContent>
+    </Modal>
+  );
+}
