d485939ff219809be8421e743ebff90d6ec26f4c
pedrozzneo <pedrozzneo@gmail.com>
2026-04-23 18:15:03 -0300

    feat: add ternary to display there are no researchers with that filter

--- src/features/review/planning-protocol/pages/GeneralDefinition/subcomponents/ResearcherFilter/AddResearcher.tsx
+++ src/features/review/planning-protocol/pages/GeneralDefinition/subcomponents/ResearcherFilter/AddResearcher.tsx
@@ -25,16 +25,21 @@ export default function AddResearcher() {
         <Input placeholder="Add a researcher" flex="1" minW={0} size="md" onChange = {filterPotentialResearchers} onFocus={() => setSuggestionsOpen(true)} onBlur={() => setSuggestionsOpen(false)}/>
         <EventButton w="40px" flexShrink={0} />
         {suggestionsOpen && (
-          <Box position="absolute" width="25rem" top="100%" mt={1} bg="white" border="1px solid" borderColor="gray.300" borderRadius="md">
-            {selectedPotentialResearchers.map((researcher) => (
-              <Flex key={researcher.id} align="center" gap={3} px={3} py={2}>
-              <Avatar size="sm" name="Potential Researcher 1" />
-                <Text flex="1" fontSize="sm">{researcher.name} - {researcher.email}</Text>
-              </Flex>
-            ))}
-          </Box>
+            <Box position="absolute" width="25rem" top="100%" mt={1} bg="white" border="1px solid" borderColor="gray.300" borderRadius="md">
+              {selectedPotentialResearchers.length > 0 ? (
+                selectedPotentialResearchers.map((researcher) => (
+                  <Flex key={researcher.id} align="center" gap={3} px={3} py={2}>
+                    <Avatar size="sm" name="Potential Researcher 1" />
+                    <Text flex="1" fontSize="sm">{researcher.name} - {researcher.email}</Text>
+                  </Flex>
+                ))
+              ) : (
+                <Text color="gray.500" textAlign="center">No researchers found</Text>
+              )
+            }
+        </Box>
         )}
       </Flex>
     </Flex>
-  );
+  )
 }
