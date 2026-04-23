7833f2983873881e40325f741a0fe49e3ebb26e2
pedrozzneo <pedrozzneo@gmail.com>
2026-04-23 16:52:05 -0300

    styles: gap between avatar icon and name

--- src/features/review/planning-protocol/pages/GeneralDefinition/subcomponents/ResearcherFilter/IncludedResearchers.tsx
+++ src/features/review/planning-protocol/pages/GeneralDefinition/subcomponents/ResearcherFilter/IncludedResearchers.tsx
@@ -4,9 +4,9 @@ import { Flex, Icon, Text, Avatar } from "@chakra-ui/react";
 export default function IncludedResearchers() {
   return (
     <>
-      <Flex align="center" gap={4} px={4} py={2} borderWidth="1px" borderColor="gray.200" borderRadius="md">
+      <Flex align="center" gap={5} px={4} py={2} borderWidth="1px" borderColor="gray.200" borderRadius="md">
         <Avatar size="sm" name="Ana Silva" />
-        <Flex borderColor="gray.200" borderRadius="md"  align="center" justify="space-between" flex="1">
+        <Flex align="center" justify="space-between" flex="1">
           <Text>Ana Silva - ana.silva@gmail.com</Text>
           <Icon as={DeleteIcon} w="15px" h="15px" color="gray.500" />
         </Flex>
