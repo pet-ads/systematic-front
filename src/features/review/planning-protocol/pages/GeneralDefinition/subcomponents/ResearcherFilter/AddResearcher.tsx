6f887c048718701ad81bea47aed96ecea1fb405d
pedrozzneo <pedrozzneo@gmail.com>
2026-04-23 18:00:37 -0300

    feat: map through all the potential researchers

--- src/features/review/planning-protocol/pages/GeneralDefinition/subcomponents/ResearcherFilter/AddResearcher.tsx
+++ src/features/review/planning-protocol/pages/GeneralDefinition/subcomponents/ResearcherFilter/AddResearcher.tsx
@@ -1,9 +1,16 @@
 import { Flex, Input, Box, Avatar, Text } from "@chakra-ui/react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import EventButton from "@components/common/buttons/EventButton";
+import { potentialResearchersMock } from "../../../../../../../mocks/potentialResearchers";
 
 export default function AddResearcher() {
+  const [potentialResearchers, setPotentialResearchers] = useState(potentialResearchersMock);
   const [suggestionsOpen, setSuggestionsOpen] = useState(false);
+
+  useEffect(() => {
+    // simulating API call to get potential researchers
+    setPotentialResearchers(potentialResearchersMock);
+  }, []);
 
   return (
     <Flex justify="center" py={2}>
@@ -12,10 +19,12 @@ export default function AddResearcher() {
         <EventButton w="40px" flexShrink={0} />
         {suggestionsOpen && (
           <Box position="absolute" width="25rem" top="100%" mt={1} bg="white" border="1px solid" borderColor="gray.300" borderRadius="md">
-            <Flex align="center" gap={3} px={3} py={2}>
+            {potentialResearchers.map((researcher) => (
+              <Flex key={researcher.id} align="center" gap={3} px={3} py={2}>
               <Avatar size="sm" name="Potential Researcher 1" />
-              <Text flex="1" fontSize="sm">Potential Researcher 1 - unincluded1@gmail.com</Text>
-            </Flex>
+                <Text flex="1" fontSize="sm">{researcher.name} - {researcher.email}</Text>
+              </Flex>
+            ))}
           </Box>
         )}
       </Flex>
