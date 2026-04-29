1d9cba233c2c75a5f583e89834863f68f36c28bf
pedrozzneo <pedrozzneo@gmail.com>
2026-04-29 16:25:24 -0300

    feat: add delete function

--- src/features/review/planning-protocol/pages/GeneralDefinition/subcomponents/ResearcherFilter/IncludedResearchers.tsx
+++ src/features/review/planning-protocol/pages/GeneralDefinition/subcomponents/ResearcherFilter/IncludedResearchers.tsx
@@ -11,6 +11,16 @@ export default function IncludedResearchers() {
     setAddedResearchers(addedResearchersMock);
   }, []);
 
+  const handleDelete = (id: string) => {
+    setAddedResearchers(
+      addedResearchers.map((researcher) =>
+        researcher.id === id
+          ? { ...researcher, status: "excluding" }
+          : researcher,
+      ),
+    );
+  };
+
   return (
     <>
     {addedResearchers.map((researcher) => (
@@ -19,13 +29,13 @@ export default function IncludedResearchers() {
         <Flex align="center" justify="space-between" flex="1">
           <Text>{researcher.name} - {researcher.email}</Text>
           <Flex align="center" gap={5}>
-            {(researcher.status === "pending" || researcher.status === "expired") && (
+            {(researcher.status === "pending" || researcher.status === "expired" || researcher.status === "excluding") && (
               <Text color="gray.500">
                 {researcher.status.charAt(0).toUpperCase() + researcher.status.slice(1)}
               </Text>
             )}
             {researcher.status == "included" && (<Text>Role: {researcher.role}</Text>)}
-            <Button variant="ghost">
+            <Button variant="ghost" onClick={() => handleDelete(researcher.id)}>
               <Icon as={DeleteIcon} w={"15px"} h={"15px"} />
             </Button>
           </Flex> 
