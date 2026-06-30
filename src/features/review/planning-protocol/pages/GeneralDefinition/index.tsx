6d3b8eb3bced3857f8af47357014a4538ca4c2b0
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-06-30 09:48:13 -0300

    resolve conflicts between CP-ui-large-screen-responsiveness and test-clean

--- src/features/review/planning-protocol/pages/GeneralDefinition/index.tsx
+++ src/features/review/planning-protocol/pages/GeneralDefinition/index.tsx
@@ -95,8 +95,8 @@ export default function GeneralDefinition() {
           <AlertInputText
             border="red"
             value={title}
-            label="Title is required"
-            placeholder="Enter review title"
+            label={t("generalDefinition.alert.title.label")}
+            placeholder={t("generalDefinition.alert.title.placeholder")}
             type="text"
             nome="text"
             onChange={(event) =>
@@ -118,8 +118,8 @@ export default function GeneralDefinition() {
           <AlertInputText
             border="red"
             value={description}
-            label="Description is required"
-            placeholder="Enter review description"
+            label={t("generalDefinition.alert.description.label")}
+            placeholder={t("generalDefinition.alert.description.placeholder")}
             type="text"
             nome="text"
             onChange={(event) =>
@@ -141,8 +141,8 @@ export default function GeneralDefinition() {
           <AlertInputText
             border="red"
             value={objectives}
-            label="Objectives are required"
-            placeholder="What are your goals?"
+            label={t("generalDefinition.alert.objectives.label")}
+            placeholder={t("generalDefinition.alert.objectives.placeholder")}
             type="text"
             nome="text"
             onChange={(event) =>
